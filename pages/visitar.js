cd1a58d747fdfe6cc63e334edef8ca1b50bc93ec
Alfonsoesq <100050669+Alfonsoesq@users.noreply.github.com>
2025-08-07 18:22:50 -0600

    Save current work before creating new branch

--- pages/visitar.js
+++ pages/visitar.js
@@ -15,16 +15,16 @@ export default function Visitar() {
         <h1 className="text-3xl font-bold mb-6">Planifica tu Visita</h1>
         <p className="mb-4">Nuestro horario de servicios es:</p>
         <ul className="list-disc list-inside mb-6">
-          <li>Domingo: 10:00 AM y 6:00 PM</li>
+          <li>Domingo: 10:00 AM</li>
           <li>Miércoles: 7:00 PM</li>
         </ul>
         <p>Estamos ubicados en:</p>
         <address className="not-italic mb-6">
-          Calle Principal 123,<br />
-          Guadalajara, Jalisco, México
+          Durazno 3A,<br />
+          Las Huertas, Tlaquepaque, Jalisco.
         </address>
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3732.343591060438!2d-103.34960968480914!3d20.659699906263154!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8428b55bb354b4db%3A0x2a86d1dca5ecaf11!2sGuadalajara%2C%20Jalisco%2C%20M%C3%A9xico!5e0!3m2!1ses!2sus!4v1691268453773!5m2!1ses!2sus"
+          src=" https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3732.343591060438!2d-103.34960968480914!3d20.659699906263154!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8428b55bb354b4db%3A0x2a86d1dca5ecaf11!2sGuadalajara%2C%20Jalisco%2C%20M%C3%A9xico!5e0!3m2!1ses!2sus!4v1691268453773!5m2!1ses!2sus"
           width="100%"
           height="300"
           style={{ border: 0 }}
